bf7dcf9d6d49267cdf847681cd509dc17fd2b66e
Peter Oettig <peet1993@gmail.com>
2023-05-27 16:30:16 +0200

    Added config path by command line, PEP compliancy

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import argparse
 import json
 import logging
 import time
-
 import requests
 
 from prometheus_client import start_http_server, Gauge
@@ -12,21 +12,81 @@ API_URL = f"{BASEURL}{API_PATH}"
 
 labels = ["user_id", "username"]
 gauges = {
-	"total_hits": Gauge("osu_total_hitobjects_hit", "Total number of hitobjects hit by the user on maps with a leaderboard.", labels),
-	"play_count": Gauge("osu_playcount", "Number of plays done by the user, including fails and retries, on a map with a leaderboard.", labels),
-	"ranked_score": Gauge("osu_ranked_score", "Total of every best score achieved by the user on a map with a leaderboard.", labels),
-	"total_score": Gauge("osu_total_score", "Total of every score achieved by the user on a map with a leaderboard.", labels),
-	"global_rank": Gauge("osu_rank", "Rank of the user on the global pp leaderboard.", labels),
-	"country_rank": Gauge("osu_rank_country", "Rank of the user on the country-based pp leaderboard.", labels),
-	"level": Gauge("osu_level", "The level of the user.", labels),
-	"pp": Gauge("osu_pp", "The pp score of the user.", labels),
-	"hit_accuracy": Gauge("osu_accuracy", "The accuracy of the user averaged over all plays, better plays are weighted more (like with pp).", labels),
-	"grade_counts_ss": Gauge("osu_ss_count", "The number of SS plays achieved by the user.", labels),
-	"grade_counts_ssh": Gauge("osu_ss_modded_count", "The number of modded SS plays achieved by the user.", labels),
-	"grade_counts_s": Gauge("osu_s_count", "The number of S plays achieved by the user.", labels),
-	"grade_counts_sh": Gauge("osu_s_modded_count", "The number of modded S plays achieved by the user.", labels),
-	"grade_counts_a": Gauge("osu_a_count", "The number of A plays achieved by the user.", labels),
-	"play_time": Gauge("osu_total_seconds_played", "The total number of seconds the user was actively playing a map.", labels),
+	"total_hits": Gauge(
+		"osu_total_hitobjects_hit",
+		"Total number of hitobjects hit by the user on maps with a leaderboard.",
+		labels
+	),
+	"play_count": Gauge(
+		"osu_playcount",
+		"Number of plays done by the user, including fails and retries, on a map with a leaderboard.",
+		labels
+	),
+	"ranked_score": Gauge(
+		"osu_ranked_score",
+		"Total of every best score achieved by the user on a map with a leaderboard.",
+		labels
+	),
+	"total_score": Gauge(
+		"osu_total_score",
+		"Total of every score achieved by the user on a map with a leaderboard.",
+		labels
+	),
+	"global_rank": Gauge(
+		"osu_rank",
+		"Rank of the user on the global pp leaderboard.",
+		labels
+	),
+	"country_rank": Gauge(
+		"osu_rank_country",
+		"Rank of the user on the country-based pp leaderboard.",
+		labels
+	),
+	"level": Gauge(
+		"osu_level",
+		"The level of the user.",
+		labels
+	),
+	"pp": Gauge(
+		"osu_pp",
+		"The pp score of the user.",
+		labels
+	),
+	"hit_accuracy": Gauge(
+		"osu_accuracy",
+		"The accuracy of the user averaged over all plays, better plays are weighted more (like with pp).",
+		labels
+	),
+	"grade_counts_ss": Gauge(
+		"osu_ss_count",
+		"The number of SS plays achieved by the user.",
+		labels
+	),
+	"grade_counts_ssh": Gauge(
+		"osu_ss_modded_count",
+		"The number of modded SS plays achieved by the user.",
+		labels
+	),
+	"grade_counts_s": Gauge(
+		"osu_s_count",
+		"The number of S plays achieved by the user.",
+		labels
+	),
+	"grade_counts_sh": Gauge(
+		"osu_s_modded_count",
+		"The number of modded S plays achieved by the user.",
+		labels
+	),
+	"grade_counts_a": Gauge(
+		"osu_a_count",
+		"The number of A plays achieved by the user.",
+		labels
+	),
+	"play_time": Gauge(
+		"osu_total_seconds_played",
+		"The total number of seconds the user was actively playing a map.",
+		labels
+	),
 }
 
 
@@ -73,8 +133,16 @@ def request_user_data(access_token, user_id):
 
 
 def main():
+	parser = argparse.ArgumentParser(description="Converts stats of osu users into prometheus metrics")
+	parser.add_argument(
+		"--config", "-c",
+		default="config.json",
+		help="Path to the config file. Defaults to 'config.json' in the current working directory."
+	)
+	args = parser.parse_args()
+
 	logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
-	with open("config.json", "r") as fh:
+	with open(args.config, "r") as fh:
 		config = json.load(fh)
 
 	error_tracker = ErrorTracker(config["max_intervals_with_errors"])
@@ -97,14 +165,15 @@ def main():
 				response = request_user_data(access_token, user_id)
 
 				if response.status_code == 401:
-					error_tracker.process_error(f"Authentication still denied after fetching new token. Aborting upate.")
+					error_tracker.process_error(f"Authentication still denied after fetching new token. Aborting update.")
 					break
 
 			if response.status_code != 200:
-				logging.warning(f"Failed to fetch user info for user id {user_id}: {response.status_code} - {response.content.decode('utf-8')}")
+				logging.warning(
+					f"Failed to fetch user info for user id {user_id}: "
+					f"{response.status_code} - {response.content.decode('utf-8')}"
+				)
 				continue
-
-			error_intervals = 0
 
 			# Convert stats to workable format
 			data = response.json()
